ec5bb8227b5212e94425740dcb241817db979362
franTarkenton <fran.tarkington@gmail.com>
2013-11-29 16:47:33 -0800

    Still trying to figure out the WFS query

--- learning_web_proj/cum_effects/SimpleMap_modular_v2.js
+++ learning_web_proj/cum_effects/SimpleMap_modular_v2.js
@@ -12,6 +12,8 @@ var maplib = ( function() {
         var drawingLayer;
         var gce_ip = '108.59.81.196';
         var gce_wfs = "http://" + gce_ip + "/geoserver/wfs"
+        var muleDeerWFS;
+        var muleDeerProtocolWFS;
 
         var wfsStyleMap = new OpenLayers.StyleMap({
             "default" : new OpenLayers.Style({
@@ -20,13 +22,6 @@ var maplib = ( function() {
                 fillOpacity : .2
             })
         });
-
-        // example of local or private function:
-        function someFunc() {
-            var somevar = 1;
-            // local variable
-            console.log("somevar is now 1");
-        };
 
         function layerExists(lyrName) {
             var retVal = false;
@@ -94,30 +89,191 @@ var maplib = ( function() {
 
         };
 
+        function addCumEffects_WFS(addToMap) {
+            muleDeerProtocolWFS = new OpenLayers.Protocol.WFS({
+                url : gce_wfs,
+                version : "1.1.0",
+                featureType : "mule_deer",
+      
+            });
+            muleDeerWFS = new OpenLayers.Layer.Vector("WFS", {
+                styleMap : wfsStyleMap,
+                strategies : [new OpenLayers.Strategy.BBOX()],
+                protocol : muleDeerProtocolWFS,
+                projection : new OpenLayers.Projection("EPSG:900913") //This is how the coordinates will be served
+            });
+            if (addToMap === true) {
+                map.addLayer(muleDeerWFS);
+            }
+        }
+
         /**
          * Going to add the cumulative effects WFS layers, but
          * they will be added as wms's with query capability for
          * now.
          */
         function addCumEffectsLayers_WMS() {
-            var protocol = new OpenLayers.Protocol.WFS({
-                url : gce_wfs,
-                version : "1.1.0",
-                featureType : "study_bndry",
+
+            var cumEffectsStudyArea = new OpenLayers.Layer.WMS("cum_effects:study_bndry - Tiled", "http://108.59.81.196:8080/geoserver/cum_effects/wms", {
+                LAYERS : 'cum_effects:study_bndry',
+                STYLES : '',
+                format : 'image/png',
+                tiled : true,
+                tilesOrigin : map.maxExtent.left + ',' + map.maxExtent.bottom,
+                transparent : true
+            }, {
+                opacity : 0.35,
+                buffer : 0,
+                displayOutsideMaxExtent : true,
+                isBaseLayer : false,
+                yx : {
+                    'EPSG:3005' : false
+                }
             });
-            var studyAreaWFS = new OpenLayers.Layer.Vector("WFS", {
-                styleMap : wfsStyleMap,
-                strategies : [new OpenLayers.Strategy.BBOX()],
-                protocol : protocol,
-                projection : new OpenLayers.Projection("EPSG:4326") //telling the wfs how I want the coords served, ie what proj to get returned as
+            map.addLayer(cumEffectsStudyArea);
+
+            var cumEffectsMuleDeer = new OpenLayers.Layer.WMS("cum_effects:mule_deer - Tiled", "http://108.59.81.196:8080/geoserver/cum_effects/wms", {
+                LAYERS : 'cum_effects:mule_deer',
+                STYLES : '',
+                format : 'image/png',
+                tiled : true,
+                tilesOrigin : map.maxExtent.left + ',' + map.maxExtent.bottom,
+                transparent : true
+            }, {
+                opacity : 0.35,
+                buffer : 0,
+                displayOutsideMaxExtent : true,
+                isBaseLayer : false,
+                yx : {
+                    'EPSG:3005' : false
+                }
             });
-            map.addLayer(studyAreaWFS);
+            map.addLayer(cumEffectsMuleDeer);
         }
 
+        /**
+         * 1) iterates through the features provided as an arg
+         * 2) Uses JSTS to calculate portions that are inside
+         *    submitted polygon,
+         * 3) adds the intersected features to a new layer
+         * 4) draws the new layer
+         */
+        function calcIntersection(features) {
+            // TODO: need to come back and add logic to make sure that we actually have a drawing feature to work with
+            console.log("processing this number of features: " + features.length);
+            var jstsReader = new jsts.io.WKTReader();
 
-        mapObj.printSomething = function printSomething() {
-            console.log('hello got here hello');
+            var intersectFeature = drawingLayer.features[0];
+            var intersectGeomString = intersectFeature.geometry.toString();
+            var intersectJSTSGeom = jstsReader.read(intersectGeomString);
+            for (var i = 0; i < features.length; i += 1) {
+                // first test to see of the geometry intersects, if it
+                // does then we will calculate the portions that are
+                // insdie the drawing polygon.
+
+                // if there is an intersection should then test for contains.
+                // if the poly is contained then we do not have to determine
+                // the portions that intersect.
+                console.log("feature count: "  + features[i].geometry);
+                var jstsGeom = jstsReader.read(features[i].geometry.toString());
+                // is it contained?
+                if (intersectJSTSGeom.contains(intersectJSTSGeom)) {
+                    // just add it to the new layer
+                    console.log("feature is contained!");
+                } else if (intersectJSTSGeom.intersects(intersectJSTSGeom)) {
+                    console.log("intersects!");
+                } else {
+                    console.log("Toss it!");
+                }
+            }
+            console.log("finished!")
+        }
+
+        function test_adddummyPolygon() {
+            if ( typeof (drawingLayer) === "undefined") {
+                addDrawingLayer();
+            }
+            var wktParser = new OpenLayers.Format.WKT();
+            var wktpoly = "POLYGON((-13454551.096475 6371596.6845817,-13448206.823128 6372208.1808079,-13439531.220419 6369838.6329314,-13434983.217236 6363723.6706694,-13450347.05992 6359443.197086,-13454551.096475 6371596.6845817))";
+            var feat1 = wktParser.read(wktpoly);
+            drawingLayer.addFeatures([feat1]);
+        }
+
+        /**
+         * Remove all the polygons in the drawing layer
+         */
+        mapObj.clearAOI = function clearAOI() {
+            drawingLayer.removeAllFeatures();
         };
+
+        mapObj.reportOnDrawing = function reportOnDrawing() {
+            // get the information underneath the drawn polygon with a
+            // wfs query in a web worker
+            // A) define a filter
+            addCumEffects_WFS();
+            test_adddummyPolygon();
+            var drawingGeom = drawingLayer.features[0].geometry;
+            console.log("drawinglayer geom is:" + drawingGeom);
+
+            var spatialFilterParams = {
+                type : OpenLayers.Filter.Spatial.INTERSECTS,
+                value : drawingGeom, 
+                projection: new OpenLayers.Projection("EPSG:900913")
+
+            };
+            var filter_spatial = new OpenLayers.Filter.Spatial(spatialFilterParams);
+            
+            
+            var compObj = {
+                type: OpenLayers.Filter.Comparison.NOT_EQUAL_TO, 
+                property: 'RISK', 
+                value: 'NOT APPLICABLE'
+            };
+            var filter_conditional = new OpenLayers.Filter.Comparison(compObj);
+            var allfilters = [filter_spatial, filter_conditional]
+            var filterComb = new OpenLayers.Filter.Logical(
+                {
+                    filters:allfilters,
+                    type: OpenLayers.Filter.Logical.AND, 
+                });
+            
+            console.log("filter enabled!");
+
+            // going to read the features from the vector layer that
+            // wraps the wfs protocol.  Down the road look into whether it is
+            // significantly more efficient to read straight from the protocol object.
+
+            muleDeerProtocolWFS.read({
+                filter: filterComb,
+                callback: WFSReadCallBack
+            });
+        }
+        
+        function objectToConsole(inObj) {
+            var keys = Object.keys(inObj);
+            for (var i=0; i<keys.length; i++) {
+                console.log(keys[i] + ' = ' + inObj[keys[i]]);
+            }
+        }
+        
+        function WFSReadCallBack(response) {
+            // TODO: should come back and add a visual widget to show that processing is taking place behind the scenes.
+            // read from the response and send the features to the 
+            // intersect method.
+            if (response.error) {
+                //TODO: should figure out what to do if an error is encountered here. Exceptions?
+                objectToConsole(response)
+                console.log("error with the WFS query");
+                console.log(response.error);
+                console.log(response.error.exceptionReport.exceptions[0]);
+                console.log(response.error.exceptionReport.exceptions[0].texts[0]);
+            }
+            console.log("Callback called!");
+            
+            console.log("elements in response: " + response.features.length);
+            // now send the features to the intersection method
+            calcIntersection(response.features);
+        }
 
         mapObj.drawAOI = function drawAOI(drawMethod) {
             if ( typeof (drawingLayer) === "undefined") {
